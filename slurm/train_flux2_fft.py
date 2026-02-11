2c7c1ec1769856e935fb7b05fa3a4a9d5553b9b5
jkdxg8837 <jkdxg8837@gmail.com>
2026-02-11 15:07:41 +0000

    flux 2 script

---
+++ slurm/train_flux2_fft.py
@@ -0,0 +1,33 @@
+import subprocess
+
+model_name = "black-forest-labs/FLUX.2-klein-4B"
+instance_dir = "dog"
+output_dir = "trained-flux2-klein"
+
+cmd = [
+    "accelerate", "launch", 
+    "--config_file", "../accelerate_config_zero2.yaml",
+    "../train_dreambooth_lora_flux2_klein.py",
+    "--do_fp8_training",
+    "--pretrained_model_name_or_path", model_name,
+    "--instance_data_dir", instance_dir,
+    "--output_dir", output_dir,
+    # "--mixed_precision", "bf16",
+    "--instance_prompt", "a photo of sks dog",
+    "--resolution", "512",
+    "--train_batch_size", "1",
+    "--guidance_scale", "1",
+    "--gradient_accumulation_steps", "4",
+    "--optimizer", "adamW",
+    "--use_8bit_adam",
+    "--learning_rate", "1e-5",
+    "--report_to", "wandb",
+    "--lr_scheduler", "constant",
+    "--lr_warmup_steps", "0",
+    "--max_train_steps", "500",
+    "--seed", "0",
+    "--cache_latents",
+    "--repeats","8",
+]
+
+subprocess.run(cmd, check=True)
